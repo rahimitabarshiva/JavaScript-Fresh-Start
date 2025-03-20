f3a8695a7aea59d969fdd75c3d9c87cb03717c77
Shiva Rahimitabar 🙋‍♀️👩‍💻 <rahimitabarshiva@gmail.com>
2025-03-20 20:01:00 +0330

    Developed by Shiva Rahimitabar
    Date : 3/20/2025

---
+++ assets/JavaScript/master.js
@@ -0,0 +1,26 @@
+let firstName = prompt(`Enter your first name, please!`);
+console.log(firstName);
+let lastName = prompt(`Enter your last name, please!!!`);
+console.log(lastName);
+document.write(`<strong id="first">Hello dear Mr/Miss ${firstName} ${lastName}!☺️</strong>`);
+document.getElementById("first").style.fontSize = `60px`
+document.write(`<hr id="second">`);
+document.getElementById("second").style.border = `5px solid navy`
+document.write(`<span id="third">Welcome to my website!!!🥰</span>`)
+document.getElementById("third").style.fontSize = `60px`
+document.getElementById("third").style.color = `yellow`
+document.getElementById("third").style.display = `flex`
+document.getElementById("third").style.width = `100%`
+document.getElementById("third").style.justifyContent = `center`
+document.write(`<span id="fourth">💗💗💗💗</span>`);
+document.getElementById("fourth").style.fontSize = `80px`
+document.getElementById("fourth").style.display = `flex`
+document.getElementById("fourth").style.width = `100%`
+document.getElementById("fourth").style.justifyContent = `center`
+
+
+
+
+
+
+
